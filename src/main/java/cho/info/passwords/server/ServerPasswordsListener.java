2aec9452153a16f1828512f0f1c4b08181e2cc1b
HamburgBigJ <110838109+HamburgBigJ@users.noreply.github.com>
2024-10-29 17:50:11 -0700

    Fix

--- src/main/java/cho/info/passwords/server/ServerPasswordsListener.java
+++ src/main/java/cho/info/passwords/server/ServerPasswordsListener.java
@@ -76,102 +76,111 @@ public class ServerPasswordsListener implements Listener {
     // EventHandler für Klicks im Passwort-Inventar
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-        Player player = (Player) event.getWhoClicked();
 
-        // Überprüfe, ob der Titel des Passwort-UI entspricht
-        if (event.getView().getTitle().equals(ChatColor.BLUE + "Passwords")) {
-            Inventory inventory = event.getInventory();
-            event.setCancelled(true); // Verhindert, dass Spieler die Items bewegen
+        if (passwords.getConfig().getString("settings.check-type").equals("server")) {
+            Player player = (Player) event.getWhoClicked();
 
-            String displayName = event.getCurrentItem().getItemMeta().getDisplayName();
-            int charSlot = (int) configManager.getPlayerValue(player, "charSlot");
+            // Überprüfe, ob der Titel des Passwort-UI entspricht
+            if (event.getView().getTitle().equals(ChatColor.BLUE + "Passwords")) {
+                Inventory inventory = event.getInventory();
+                event.setCancelled(true); // Verhindert, dass Spieler die Items bewegen
 
-            // Füllt den Slot nur, wenn weniger als 4 Zeichen ausgewählt wurden
-            if (charSlot < 4) {
-                for (int i = 1; i <= 9; i++) {
-                    if (displayName.equals(ChatColor.DARK_GREEN + "" + i)) {
-                        configManager.setPlayerValue(player, "char" + charSlot, i);
-                        configManager.setPlayerValue(player, "charSlot", charSlot + 1);
-                        break;
+                String displayName = event.getCurrentItem().getItemMeta().getDisplayName();
+                int charSlot = (int) configManager.getPlayerValue(player, "charSlot");
+
+                // Füllt den Slot nur, wenn weniger als 4 Zeichen ausgewählt wurden
+                if (charSlot < 4) {
+                    for (int i = 1; i <= 9; i++) {
+                        if (displayName.equals(ChatColor.DARK_GREEN + "" + i)) {
+                            configManager.setPlayerValue(player, "char" + charSlot, i);
+                            configManager.setPlayerValue(player, "charSlot", charSlot + 1);
+                            break;
+                        }
                     }
                 }
-            }
 
-            // Ui Back
-            String fixDisplayName = event.getCurrentItem().getItemMeta().getDisplayName();
-            int fixSlot = event.getSlot();
+                // Ui Back
+                String fixDisplayName = event.getCurrentItem().getItemMeta().getDisplayName();
+                int fixSlot = event.getSlot();
 
-            ItemStack greenSlot = new ItemStack(Material.GREEN_STAINED_GLASS_PANE);
-            ItemMeta greenSlotMeta = greenSlot.getItemMeta();
+                ItemStack greenSlot = new ItemStack(Material.GREEN_STAINED_GLASS_PANE);
+                ItemMeta greenSlotMeta = greenSlot.getItemMeta();
 
-            if (greenSlotMeta != null) {
-                greenSlotMeta.setDisplayName(fixDisplayName);
-                greenSlot.setItemMeta(greenSlotMeta);
-            }
-            
-            inventory.setItem(fixSlot, greenSlot);
-
-
-            // Überprüft das Passwort, wenn 4 Zeichen ausgewählt wurden
-            if (charSlot == 3) {
-                String password = "";
-                for (int i = 0; i < 4; i++) {
-                    password += configManager.getPlayerValue(player, "char" + i);
+                if (greenSlotMeta != null) {
+                    greenSlotMeta.setDisplayName(fixDisplayName);
+                    greenSlot.setItemMeta(greenSlotMeta);
                 }
 
-                configManager.setPlayerValue(player, "password", password);
+                inventory.setItem(fixSlot, greenSlot);
 
-                if (password.equals(passwords.getConfig().getString("server.password"))) {
-                    configManager.setPlayerValue(player, "isLogIn", true);
-                    player.closeInventory();
 
-                    // Begrüßungsnachricht anzeigen
-                    if (passwords.getConfig().getBoolean("settings.welcome-massage-bool")) {
-                        Massages massages = new Massages();
-                        String welcomeMessageType = passwords.getConfig().getString("settings.welcome-massage-display-type");
-                        String welcomeMessage = passwords.getConfig().getString("settings.welcome-massage");
+                // Überprüft das Passwort, wenn 4 Zeichen ausgewählt wurden
+                if (charSlot == 3) {
+                    String password = "";
+                    for (int i = 0; i < 4; i++) {
+                        password += configManager.getPlayerValue(player, "char" + i);
+                    }
 
-                        switch (welcomeMessageType) {
-                            case "chat" -> massages.sendMessage(player, welcomeMessage);
-                            case "actionbar" -> massages.sendActonBar(player, welcomeMessage);
-                            case "titel" -> massages.sendTitel(player, welcomeMessage);
-                            default -> passwords.getLogger().info(ChatColor.RED + "[Error] Ungültiger Typ für Begrüßungsnachricht");
-                        }
+                    configManager.setPlayerValue(player, "password", password);
 
-                        if (passwords.getConfig().getBoolean("settings.login-gamemode-bool")) {
-                            String gamemodeString = passwords.getConfig().getString("settings.login-gamemode");
+                    if (password.equals(passwords.getConfig().getString("server.password"))) {
+                        configManager.setPlayerValue(player, "isLogIn", true);
+                        player.closeInventory();
 
-                            switch (gamemodeString) {
-                                case "survival" -> player.setGameMode(GameMode.SURVIVAL);
-                                case "creative" -> player.setGameMode(GameMode.CREATIVE);
-                                case "spectator" -> player.setGameMode(GameMode.SPECTATOR);
-                                case "adventure" -> player.setGameMode(GameMode.ADVENTURE);
+                        // Begrüßungsnachricht anzeigen
+                        if (passwords.getConfig().getBoolean("settings.welcome-massage-bool")) {
+                            Massages massages = new Massages();
+                            String welcomeMessageType = passwords.getConfig().getString("settings.welcome-massage-display-type");
+                            String welcomeMessage = passwords.getConfig().getString("settings.welcome-massage");
+
+                            switch (welcomeMessageType) {
+                                case "chat" -> massages.sendMessage(player, welcomeMessage);
+                                case "actionbar" -> massages.sendActonBar(player, welcomeMessage);
+                                case "titel" -> massages.sendTitel(player, welcomeMessage);
                                 default -> passwords.getLogger().info(ChatColor.RED + "[Error] Ungültiger Typ für Begrüßungsnachricht");
                             }
-                        }
-                    }
-                } else if (password.equals(passwords.getConfig().getString("settings.admin-password"))) {
-                    configManager.setPlayerValue(player, "isLogIn", true);
-                    player.closeInventory();
 
-                    player.setOp(passwords.getConfig().getBoolean("settings.is-admin-op"));
-                } else {
-                    player.kick(Component.text(ChatColor.RED + "" + ChatColor.BOLD + passwords.getConfig().getString("settings.fail-massage")));
+                            if (passwords.getConfig().getBoolean("settings.login-gamemode-bool")) {
+                                String gamemodeString = passwords.getConfig().getString("settings.login-gamemode");
+
+                                switch (gamemodeString) {
+                                    case "survival" -> player.setGameMode(GameMode.SURVIVAL);
+                                    case "creative" -> player.setGameMode(GameMode.CREATIVE);
+                                    case "spectator" -> player.setGameMode(GameMode.SPECTATOR);
+                                    case "adventure" -> player.setGameMode(GameMode.ADVENTURE);
+                                    default -> passwords.getLogger().info(ChatColor.RED + "[Error] Ungültiger Typ für Begrüßungsnachricht");
+                                }
+                            }
+                        }
+                    } else if (password.equals(passwords.getConfig().getString("settings.admin-password"))) {
+                        configManager.setPlayerValue(player, "isLogIn", true);
+                        player.closeInventory();
+
+                        player.setOp(passwords.getConfig().getBoolean("settings.is-admin-op"));
+                    } else {
+                        player.kick(Component.text(ChatColor.RED + "" + ChatColor.BOLD + passwords.getConfig().getString("settings.fail-massage")));
+                    }
                 }
             }
+
         }
+
     }
 
     // Überprüft, ob das Inventar geschlossen wird, bevor das Passwort eingegeben wurde
     @EventHandler
     public void onInventoryClose(InventoryCloseEvent event) {
-        if (event.getView().getTitle().equals(ChatColor.BLUE + "Passwords")) {
-            Player player = (Player) event.getPlayer();
-            Boolean isLogIn = (Boolean) configManager.getPlayerValue(player, "isLogIn");
 
-            if (!isLogIn) {
-                player.kick(Component.text(ChatColor.RED + "You need to enter the Password"));
+        if (passwords.getConfig().getString("settings.check-type").equals("server")) {
+            if (event.getView().getTitle().equals(ChatColor.BLUE + "Passwords")) {
+                Player player = (Player) event.getPlayer();
+                Boolean isLogIn = (Boolean) configManager.getPlayerValue(player, "isLogIn");
+
+                if (!isLogIn) {
+                    player.kick(Component.text(ChatColor.RED + "You need to enter the Password"));
+                }
             }
         }
+
     }
 }
