c1875d90966b3d6e18a2e83f86781996362295f7
HamburgBigJ <jonas@cho.info>
2025-11-02 16:55:52 +0100

    Changed Code structure of PlayerInventorySave

--- src/main/java/info/cho/passwords/Passwords.java
+++ src/main/java/info/cho/passwords/Passwords.java
@@ -14,6 +14,7 @@ import info.cho.passwords.server.PasswordPatternMode;
 import info.cho.passwords.server.PasswordServerMode;
 import info.cho.passwords.utls.DataManager;
 import info.cho.passwords.utls.PLog;
+import info.cho.passwords.utls.PlayerInventorySave;
 import info.cho.passwordsApi.password.PasswordConfig;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -80,7 +81,7 @@ public class Passwords extends JavaPlugin {
         customGui.registerGui("pattern", PasswordPatternMode.class);
 
         if (PasswordConfig.useAutoSave()) {
-            savePlayerInventory();
+            PlayerInventorySave.savePlayerInventory();
         }
 
     }
@@ -110,45 +111,5 @@ public class Passwords extends JavaPlugin {
 
         }
     }
-
-    private void savePlayerInventory() {
-        PLog.debug("Saving player inventory initialization...");
-
-        int intervalSeconds = PasswordConfig.getSavePlayerInventoryIntervall() * 60; // Convert minutes to seconds
-        int intervalTicks = intervalSeconds * 20; // Seconds to ticks (20 ticks per second)
-        PLog.debug("Saving player inventory interval: " + intervalSeconds + " seconds (" + intervalTicks + " ticks)");
-
-        getServer().getScheduler().runTaskTimer(this, () -> {
-            PLog.debug("Saving player inventory...");
-            for (Player player : Bukkit.getOnlinePlayers()) {
-                DataManager dataManager = new DataManager();
-
-                Object loginValue = dataManager.getPlayerValue(player, "isLogin");
-                boolean isLoggedIn = loginValue != null && Boolean.parseBoolean(loginValue.toString());
-                if (!isLoggedIn) {
-                    continue;
-                }
-
-                Inventory playerInventory = player.getInventory();
-                dataManager.setPlayerValue(player, "playerInventory", new ArrayList<>());
-
-                for (int i = 0; i < playerInventory.getSize(); i++) {
-                    if (playerInventory.getItem(i) != null) {
-                        ItemStack currentItem = playerInventory.getItem(i);
-                        List<Object> playerInventoryList = dataManager.getListValue(player, "playerInventory");
-                        playerInventoryList.add(currentItem);
-                        dataManager.setPlayerValue(player, "playerInventory", playerInventoryList);
-                    } else {
-                        ItemStack air = new ItemStack(Material.AIR);
-                        List<Object> playerInventoryList = dataManager.getListValue(player, "playerInventory");
-                        playerInventoryList.add(air);
-                        dataManager.setPlayerValue(player, "playerInventory", playerInventoryList);
-                    }
-                }
-            }
-        }, intervalTicks, intervalTicks);
-
-    }
-
 
 }
